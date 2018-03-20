8c5d55844232b9a9ffb624113bc6ce73d4c8c640
Aaron Crosman <aaron.crosman@cyberwoven.com>
2018-03-20 10:25:48 -0400

    Return deploy logic to Pantheon default.

--- web/private/scripts/deploy_product/prepare-for-pantheon.php
+++ web/private/scripts/deploy_product/prepare-for-pantheon.php
@@ -2,8 +2,5 @@
 
 chdir(getenv('HOME') . '/code');
 
-print "\n====== Running 'composer install' ======\n\n";
-passthru('composer install 2>&1');
-
 print "\n====== Running 'composer prepare-for-pantheon' ======\n\n";
 passthru('composer prepare-for-pantheon 2>&1');
