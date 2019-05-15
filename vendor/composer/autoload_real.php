a48ab5bf75cab258b36fdf5e53c9fedb741463f2
Drew King <drew@drewcking.com>
2019-05-15 14:18:56 -0400

    drupal 8.7.1, rabbit_hole

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit06cfba171977e2e6c39b75884dc9cee6
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit06cfba171977e2e6c39b75884dc9cee6', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        $includePaths[] = get_include_path();
+        set_include_path(implode(PATH_SEPARATOR, $includePaths));
+
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
