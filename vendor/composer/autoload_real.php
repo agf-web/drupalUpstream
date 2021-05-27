929ec52680987a136969a38497dfcf914e5c17c1
Hilary Schoenecker <hklattimore@gmail.com>
2021-05-27 13:47:28 -0400

    Drupal core security update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,10 +22,8 @@ class ComposerAutoloaderInit06cfba171977e2e6c39b75884dc9cee6
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit06cfba171977e2e6c39b75884dc9cee6', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit06cfba171977e2e6c39b75884dc9cee6', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
@@ -34,7 +32,7 @@ class ComposerAutoloaderInit06cfba171977e2e6c39b75884dc9cee6
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit06cfba171977e2e6c39b75884dc9cee6::getInitializer($loader));
         } else {
